c68d2ec4d6ad540f6360cea5bd9a9f38d8b975b7
Artem Denisov <timmy.denisov@gmail.com>
2023-07-31 22:27:52 +0300

    Initial commit

---
+++ KatishaCooker/KatishaCookerApp.swift
@@ -0,0 +1,17 @@
+//
+//  KatishaCookerApp.swift
+//  KatishaCooker
+//
+//  Created by Artem Denisov  on 2023-07-31.
+//
+
+import SwiftUI
+
+@main
+struct KatishaCookerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
