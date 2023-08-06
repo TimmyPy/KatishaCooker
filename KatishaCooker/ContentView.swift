e57b595b86903b49a19ae95683a102454d9b9fd5
Artem Denisov <timmy.denisov@gmail.com>
2023-08-06 15:17:59 +0300

    Created Timer Row View

--- KatishaCooker/ContentView.swift
+++ KatishaCooker/ContentView.swift
@@ -7,15 +7,105 @@
 
 import SwiftUI
 
-struct ContentView: View {
+class Storage {
+    
+}
+
+class TimerEntity: Identifiable {
+    @State var countDownTimer: UInt16;
+    @State var initialValue: UInt16;
+    @State var isRunning: Bool;
+    @State var name: String;
+    
+    init(countDown: UInt16) {
+        self.initialValue = countDown;
+        self.countDownTimer = countDown;
+        self.isRunning = false;
+        self.name = "Katisha Timer";
+    }
+}
+
+struct TimerLabelView: View {
+    let title: String
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        Label(title, systemImage: "bolt.fill" )
+            .labelStyle(.titleOnly)
+    }
+}
+
+struct TimerRowView: View {
+    var timer: TimerEntity
+    var body: some View {
+        HStack(){
+            // ToDo: Remove action by swipe
+            GroupBox(
+                label: TimerLabelView(title: timer.name)
+            ) {
+                Text("\(timer.countDownTimer):00")
+                    .padding(.top, 15)
+                    .frame(alignment: .leading)
+                    .font(.system(size: 50))
+            }
+            Button(
+                action: {
+                    print("\(timer.isRunning), \(timer.name)")
+                    self.timer.isRunning.toggle()
+                    print("\(self.timer.isRunning), \(timer.name)")
+                },
+                label: {
+                    Circle()
+                        .overlay(
+                            Image(systemName: self.timer.isRunning == true ? "pause.fill": "play.fill")
+                                .foregroundColor(.white)
+                        )
+                }
+            )
+            .accentColor(self.timer.isRunning == true ? .red: .blue)
+            .frame(width: 75, height: 75)
+            .background(.white)
+            .clipShape(Rectangle())
         }
-        .padding()
+    }
+}
+
+struct ContentView: View {
+    @State var timers = [
+        TimerEntity(countDown: 5),
+        TimerEntity(countDown: 15),
+        TimerEntity(countDown: 25),
+        TimerEntity(countDown: 35),
+        TimerEntity(countDown: 45),
+    ];
+    @State var countDownTimer: UInt16 = 5;
+    @State var isRunning: Bool = false;
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
+    var body: some View {
+        ZStack{
+            List{
+                ForEach(timers) { item in
+                    TimerRowView(timer: item)
+                }
+            }
+            
+            Spacer()
+            
+            Button(action: {
+                print("Plus button click")
+                timers.append(TimerEntity(countDown: 15));
+                
+            }, label: {
+                Circle()
+                    .accentColor(.cyan)
+                    .frame(width: 100, height: 100, alignment: .center)
+                    .overlay(
+                        Image(systemName: "plus")
+                            .foregroundColor(.white)
+                            .font(.system(size: 30))
+                    )
+            })
+        }
     }
 }
 
